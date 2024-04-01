020e1e2895bbf1e2a703691b6ed2161d64f90191
Agustin-dotcom <Agustin.Prieto2@alu.uclm.es>
2024-04-01 06:23:22 +0200

    Add project

---
+++ We_are_chilling.R
@@ -0,0 +1,4 @@
+rm(list=ls())
+source("C:/Users/Agus/OneDrive - Universidad de Castilla-La Mancha/Escritorio/2do año/2do cuatri/Statistics (Fernando)/R/Libraries/getMe.libraries.R")
+ls()
+getMe.libraries()
